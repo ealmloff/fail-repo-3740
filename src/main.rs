9d087c70c305d34fa1cda8a0de63ca4a8003092c
Evan Almloff <evanalmloff@gmail.com>
2025-03-25 16:36:00 -0500

    fix reproduction

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,12 @@
 use dioxus::prelude::*;
 
 fn main() {
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        #[cfg(feature = "web")]
+        // This should never run, but it does if you serve with `dx serve`
+        // It doesn't if you run with `dx serve --platform web`
+        panic!();
+    }
     launch(|| rsx!{"hi"});
 }
